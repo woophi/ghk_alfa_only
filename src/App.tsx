7e5aa755de0f57ecc80e4b09f7c5f091fa30a7df
Konstantin Mikheev <mik.kon6610@gmail.com>
2024-08-19 10:41:38 +0200

    omfg

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
 import { ButtonMobile } from '@alfalab/core-components/button/mobile';
 import { CDNIcon } from '@alfalab/core-components/cdn-icon';
 import { Gap } from '@alfalab/core-components/gap';
-import { Notification } from '@alfalab/core-components/notification';
 import { Typography } from '@alfalab/core-components/typography';
 import { useCallback, useState } from 'react';
 import alfaLogo from './assets/alfa_only_logo.png';
 
 import { CheckboxCell } from './CheckboxCell';
-import { checkboxItems, selectorItems, vipIds, visibleSpendings } from './data';
+import {
+  checkboxItems,
+  commission,
+  halfMillionIds,
+  hundredFiftyK,
+  hundredK,
+  selectorItems,
+  tenK,
+  threeMillionsIds,
+  twelveMillionsIds,
+  twoHundredK,
+} from './data';
 import { LS, LSKeys } from './ls';
 import { SelectorCell } from './SelectorCell';
 import { appSt } from './style.css';
@@ -16,37 +26,51 @@ import { sendDataToGA } from './utils/events';
 
 export const App = () => {
   const [loading, setLoading] = useState(false);
-  const [err, setError] = useState('');
   const [selectedItems, setSelected] = useState<{ id: number; title: string }[]>([]);
   const [thxShow, setThx] = useState(LS.getItem(LSKeys.ShowThx, false));
-  const [isVisible, setIsVisible] = useState(false);
 
   const selectedItemsIds = selectedItems.map(i => i.id);
-  const showSubscription = !!selectedItemsIds.length && !selectedItemsIds.some(id => vipIds.includes(id));
-  const spendings = visibleSpendings.find(
-    s => selectedItemsIds.some(id => s.ids.includes(id)) && !selectedItemsIds.some(id => s.notIds.includes(id)),
-  );
+  const showSubscription =
+    !!selectedItemsIds.filter(id => !commission.skipIds.includes(id)).length &&
+    !selectedItemsIds.some(id => commission.notIds.includes(id));
 
-  const hideNotification = useCallback(() => setIsVisible(false), []);
+  const mlns = selectedItemsIds.some(id => twelveMillionsIds.includes(id))
+    ? 12
+    : selectedItemsIds.some(id => threeMillionsIds.includes(id))
+    ? 3
+    : selectedItemsIds.some(id => halfMillionIds.includes(id))
+    ? 1.5
+    : 0;
+
+  const grands =
+    selectedItemsIds.every(id => tenK.alwaysIds.includes(id)) && selectedItemsIds.some(id => tenK.ids.includes(id))
+      ? 10
+      : selectedItemsIds.some(id => hundredFiftyK.alwaysIds.includes(id)) &&
+        !selectedItemsIds.some(id => twoHundredK.ids.includes(id))
+      ? 150
+      : selectedItemsIds.some(id => twoHundredK.ids.includes(id)) &&
+        !selectedItemsIds.some(id => twoHundredK.notIds.includes(id))
+      ? 200
+      : selectedItemsIds.some(id => hundredK.alwaysIds.includes(id)) ||
+        (selectedItemsIds.some(id => hundredK.ids.includes(id)) &&
+          !selectedItemsIds.some(id => hundredK.notIds.includes(id)))
+      ? 100
+      : 0;
+
   const submit = useCallback(() => {
-    if (!selectedItems.length || !selectedItems.some(si => si.id === 1 || si.id === 2)) {
-      setError('Выберите кэшбэк рублями');
-      setIsVisible(true);
-      return;
-    }
     setLoading(true);
     sendDataToGA({
-      cashback_rub: selectedItems.find(si => [1, 2].includes(si.id))?.title ?? '',
-      cashback_mil: selectedItems.find(si => [3, 4].includes(si.id))?.title ?? '',
-      transfers: selectedItems.find(si => [5, 6, 7, 8].includes(si.id))?.title ?? '',
-      aeroports: selectedItems.find(si => [9, 10, 11, 12].includes(si.id))?.title ?? '',
-      insurance: Number(selectedItems.some(si => si.id === 13)) as 1 | 0,
-      money_transfer: Number(selectedItems.some(si => si.id === 14)) as 1 | 0,
-      atm: Number(selectedItems.some(si => si.id === 15)) as 1 | 0,
-      concierge: Number(selectedItems.some(si => si.id === 16)) as 1 | 0,
-      stiker: Number(selectedItems.some(si => si.id === 17)) as 1 | 0,
-      metal_card: Number(selectedItems.some(si => si.id === 18)) as 1 | 0,
-      savings_account: Number(selectedItems.some(si => si.id === 19)) as 1 | 0,
+      cashback_rub: selectedItems.find(si => [1, 2, 3].includes(si.id))?.title ?? '',
+      cashback_mil: selectedItems.find(si => [4, 5, 6].includes(si.id))?.title ?? '',
+      transfers: selectedItems.find(si => [7, 8, 9, 10, 11].includes(si.id))?.title ?? '',
+      aeroports: selectedItems.find(si => [12, 13, 14, 15, 16].includes(si.id))?.title ?? '',
+      insurance: Number(selectedItems.some(si => si.id === 17)) as 1 | 0,
+      money_transfer: Number(selectedItems.some(si => si.id === 18)) as 1 | 0,
+      atm: Number(selectedItems.some(si => si.id === 19)) as 1 | 0,
+      concierge: Number(selectedItems.some(si => si.id === 20)) as 1 | 0,
+      stiker: Number(selectedItems.some(si => si.id === 21)) as 1 | 0,
+      metal_card: Number(selectedItems.some(si => si.id === 22)) as 1 | 0,
+      savings_account: Number(selectedItems.some(si => si.id === 23)) as 1 | 0,
     }).then(() => {
       LS.setItem(LSKeys.ShowThx, true);
       setThx(true);
@@ -72,7 +96,7 @@ export const App = () => {
         <div className={appSt.topContainer}>
           <img src={alfaLogo} width={158} height={38} alt="Alfa Only" />
           <Typography.Text tag="p" view="primary-medium" defaultMargins={false}>
-            Настройте свою подписку
+            Настройте свою подписку, и мы покажем условия её получения
           </Typography.Text>
 
           {selectorItems.map(item => (
@@ -115,7 +139,7 @@ export const App = () => {
           <div className={appSt.btnContainer}>
             <div>
               <Typography.TitleResponsive font="system" tag="h2" view="xsmall" weight="bold">
-                ₽ {spendings?.mln ?? 0} млн.
+                ₽ {mlns} млн.
               </Typography.TitleResponsive>
               <Typography.Text color="secondary-inverted" tag="p" view="primary-medium" defaultMargins={false}>
                 На счете
@@ -123,14 +147,16 @@ export const App = () => {
             </div>
 
             <div className={appSt.btnContainer}>
-              <div>
-                <Typography.TitleResponsive font="system" tag="h2" view="xsmall" weight="bold">
-                  ₽ {spendings?.ths ?? 0} тыс.
-                </Typography.TitleResponsive>
-                <Typography.Text color="secondary-inverted" tag="p" view="primary-medium" defaultMargins={false}>
-                  Траты в мес.
-                </Typography.Text>
-              </div>
+              {grands ? (
+                <div>
+                  <Typography.TitleResponsive font="system" tag="h2" view="xsmall" weight="bold">
+                    ₽ {grands} тыс.
+                  </Typography.TitleResponsive>
+                  <Typography.Text color="secondary-inverted" tag="p" view="primary-medium" defaultMargins={false}>
+                    Траты в мес.
+                  </Typography.Text>
+                </div>
+              ) : null}
               <div className={appSt.btnArrow}>
                 <CDNIcon name="glyph_chevron-right_m" color="#000000" />
               </div>
@@ -138,16 +164,6 @@ export const App = () => {
           </div>
         </ButtonMobile>
       </div>
-      <Notification
-        badge="negative"
-        title={err}
-        visible={isVisible}
-        offset={showSubscription ? 172 : 108}
-        onClose={hideNotification}
-        onCloseTimeout={hideNotification}
-        position="bottom"
-        className={appSt.btn}
-      />
     </>
   );
 };
--- src/data.ts
+++ src/data.ts
@@ -1,4 +1,6 @@
 import p1 from './assets/p1.png';
+import p10 from './assets/p10.png';
+import p11 from './assets/p11.png';
 import p2 from './assets/p2.png';
 import p3 from './assets/p3.png';
 import p4 from './assets/p4.png';
@@ -7,8 +9,6 @@ import p6 from './assets/p6.png';
 import p7 from './assets/p7.png';
 import p8 from './assets/p8.png';
 import p9 from './assets/p9.png';
-import p10 from './assets/p10.png';
-import p11 from './assets/p11.png';
 
 export const selectorItems: {
   img: string;
@@ -21,6 +21,7 @@ export const selectorItems: {
     items: [
       { id: 1, title: '1% за любые покупки + 7% в 4 категориях' },
       { id: 2, title: '7% в 5 категориях' },
+      { id: 3, title: 'Ничего не выбираю' },
     ],
     title: 'Кэшбэк рублями',
     titleBottomSheet: 'Выберите кэшбэк рублями',
@@ -28,8 +29,9 @@ export const selectorItems: {
   {
     img: p2,
     items: [
-      { id: 3, title: '3 мили за каждые 100 рублей любых покупок' },
-      { id: 4, title: '5 миль за каждые 100 рублей любых покупок' },
+      { id: 4, title: '3 мили за каждые 100 рублей любых покупок' },
+      { id: 5, title: '5 миль за каждые 100 рублей любых покупок' },
+      { id: 6, title: 'Ничего не выбираю' },
     ],
     title: 'Кэшбэк милями',
     titleBottomSheet: 'Выберите кэшбэк милями',
@@ -37,10 +39,11 @@ export const selectorItems: {
   {
     img: p3,
     items: [
-      { id: 5, title: '2 в месяц оформления и следующий календарный' },
-      { id: 6, title: '2 в год' },
-      { id: 7, title: '12 в год (до 2 в месяц)' },
-      { id: 8, title: '15 в год (до 3 в месяц)' },
+      { id: 7, title: '2 в месяц оформления и следующий календарный' },
+      { id: 8, title: '2 в год' },
+      { id: 9, title: '12 в год (до 2 в месяц)' },
+      { id: 10, title: '15 в год (до 3 в месяц)' },
+      { id: 11, title: 'Ничего не выбираю' },
     ],
     title: 'Поездки за наш счёт (такси, каршеринг и трансферы)',
     titleBottomSheet: 'Выберите поездки за наш счёт (такси, каршеринг и трансферы)',
@@ -48,103 +51,107 @@ export const selectorItems: {
   {
     img: p4,
     items: [
-      { id: 9, title: '2 в месяц оформления и следующий календарный' },
-      { id: 10, title: '12 в год (до 2 в месяц)' },
-      { id: 11, title: '24 в год (до 8 в месяц)' },
-      { id: 12, title: 'Без ограничений' },
+      { id: 12, title: '2 в месяц оформления и следующий календарный' },
+      { id: 13, title: '12 в год (до 2 в месяц)' },
+      { id: 14, title: '24 в год (до 8 в месяц)' },
+      { id: 15, title: 'Без ограничений' },
+      { id: 16, title: 'Ничего не выбираю' },
     ],
     title: 'Визиты в бизнес-залы и рестораны в аэропортах',
     titleBottomSheet: 'Выберите визиты в бизнес-залы и рестораны в аэропортах',
   },
 ];
 
-export const vipIds = [7, 8, 11, 12, 18];
-
-export const visibleSpendings = [
-  {
-    mln: 1.5,
-    ths: 100,
-    ids: [1, 2, 3, 5, 6, 9, 10, 13, 14, 15, 16, 17, 19],
-    notIds: [7, 11, 8, 12, 18],
-  },
-  {
-    mln: 3,
-    ths: 200,
-    ids: [7, 11],
-    notIds: [8, 12],
-  },
-  {
-    mln: 1.5,
-    ths: 150,
-    ids: [4],
-    notIds: [18],
-  },
-  {
-    mln: 3,
-    ths: 0,
-    ids: [18],
-    notIds: [19, 3, 4],
-  },
-  {
-    mln: 3,
-    ths: 100,
-    ids: [18, 19],
-    notIds: [3, 4],
-  },
-  {
-    mln: 3,
-    ths: 10,
-    ids: [18, 3],
-    notIds: [19, 4],
-  },
-  {
-    mln: 3,
-    ths: 150,
-    ids: [18, 4],
-    notIds: [19, 3],
-  },
-  {
-    mln: 12,
-    ths: 0,
-    ids: [8, 12],
-    notIds: [],
-  },
-];
-
 export const checkboxItems = [
   {
-    id: 13,
+    id: 17,
     title: 'Страхование в путешествиях для всей семьи (по РФ и за границу)',
     img: p5,
   },
   {
-    id: 14,
+    id: 18,
     title: 'Переводы (по номеру телефона / карты / реквизитов)',
     img: p6,
   },
   {
-    id: 15,
+    id: 19,
     title: 'Снятия в банкоматах любых банков',
     img: p7,
   },
   {
-    id: 16,
+    id: 20,
     title: 'Консьерж сервис 24/7 (Лайфстайл / Юридическая / Бухгалтерская / Медицинская поддержка)',
     img: p8,
   },
   {
-    id: 17,
+    id: 21,
     title: 'Выпуск платежного стикера',
     img: p9,
   },
   {
-    id: 18,
+    id: 22,
     title: 'Металлическая карта',
     img: p10,
   },
   {
-    id: 19,
+    id: 23,
     title: 'Надбавка к накопительному счету',
     img: p11,
   },
 ];
+
+export const halfMillionIds = [
+  checkboxItems[0].id,
+  checkboxItems[1].id,
+  checkboxItems[2].id,
+  checkboxItems[3].id,
+  checkboxItems[4].id,
+  checkboxItems[6].id,
+  12,
+  13,
+  7,
+  8,
+  4,
+  5,
+  1,
+  2,
+];
+
+export const threeMillionsIds = [checkboxItems[5].id, 14, 9];
+export const twelveMillionsIds = [15, 10];
+
+export const tenK = {
+  alwaysIds: [4],
+  ids: threeMillionsIds.concat(twelveMillionsIds),
+};
+
+export const hundredK = {
+  alwaysIds: [checkboxItems[6].id, 4],
+  ids: [
+    checkboxItems[0].id,
+    checkboxItems[1].id,
+    checkboxItems[2].id,
+    checkboxItems[3].id,
+    checkboxItems[4].id,
+    12,
+    13,
+    7,
+    8,
+    1,
+    2,
+  ],
+  notIds: threeMillionsIds.concat(twelveMillionsIds),
+};
+export const twoHundredK = {
+  ids: [14, 9],
+  notIds: twelveMillionsIds,
+};
+
+export const hundredFiftyK = {
+  alwaysIds: [5],
+};
+
+export const commission = {
+  notIds: [checkboxItems[5].id, 13, 14, 15, 8, 9, 10],
+  skipIds: [3, 6, 11, 16],
+};
